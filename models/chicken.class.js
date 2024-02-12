a6642620f98fd29ffe4b921bcfb2dac02528d22a
Mario Winter <mariowinter.sg@gmail.com>
2024-02-12 23:05:20 +0100

    Clouds move automatically

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -4,7 +4,7 @@ class Chicken extends MovableObject {
 			"./img/3_enemies_chicken/chicken_normal/1_walk/1_w.png"
 		);
 		this.x = 600 + Math.random() * 1960;
-		this.y = 1130;
+		this.y = 1100;
 		this.height = 148;
 		this.width = 143;
 	}
