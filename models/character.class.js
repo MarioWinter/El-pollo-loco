6460587c9ce8d673c640c767eb628a4a8d0fda7d
Mario Winter <mariowinter.sg@gmail.com>
2024-03-03 22:32:17 +0100

    Function isDead added

--- models/character.class.js
+++ models/character.class.js
@@ -23,12 +23,22 @@ class Character extends MovableObject {
 		"./img/2_character_pepe/3_jump/J-38.png",
 		"./img/2_character_pepe/3_jump/J-39.png",
 	];
+	IMGAGES_DEAD = [
+		"./img/2_character_pepe/5_dead/D-51.png",
+		"./img/2_character_pepe/5_dead/D-52.png",
+		"./img/2_character_pepe/5_dead/D-53.png",
+		"./img/2_character_pepe/5_dead/D-54.png",
+		"./img/2_character_pepe/5_dead/D-55.png",
+		"./img/2_character_pepe/5_dead/D-56.png",
+		"./img/2_character_pepe/5_dead/D-57.png",
+	];
 	walking_sound = new Audio("audio/running.mp3");
 	constructor() {
 		super().loadImage(this.IMGAGES_WALKING[0]);
 		this.x = 100;
 		this.loadImagesForMovement(this.IMGAGES_WALKING);
 		this.loadImagesForMovement(this.IMGAGES_JUMPING);
+		this.loadImagesForMovement(this.IMGAGES_DEAD);
 		this.animate();
 		this.applayGravity();
 	}
@@ -63,7 +73,9 @@ class Character extends MovableObject {
 		}, 1000 / 60);
 
 		setInterval(() => {
-			if (this.isAboveGround()) {
+			if (this.isDead()) {
+				this.playAnimation(this.IMGAGES_DEAD);
+			} else if (this.isAboveGround()) {
 				this.playAnimation(this.IMGAGES_JUMPING);
 			} else {
 				if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
