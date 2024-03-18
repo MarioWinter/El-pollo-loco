3e9b5c9b6e0135e44ad4bc546ab9ed2def81bc23
Mario Winter <mariowinter.sg@gmail.com>
2024-03-18 22:40:46 +0100

    Hitbox Bottle customised

--- models/bottle.class.js
+++ models/bottle.class.js
@@ -1,5 +1,5 @@
 class Bottle extends PickableObject {
-	y = 1130;
+	y = 1120;
 	x = 0;
 	width = 120;
 	height = 120;
@@ -8,5 +8,9 @@ class Bottle extends PickableObject {
 			"./img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png"
 		);
 		this.x = Math.random() * 2560 * 5;
+		this.offset.top = -10;
+		this.offset.bottom = 0;
+		this.offset.left = 0;
+		this.offset.right = 0;
 	}
 }
